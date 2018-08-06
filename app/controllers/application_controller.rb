985febac3d59b333df46ccfe051372b3bafe7dc5
haphuoctuyen1997 <haphuoctuyen1997@gmail.com>
2018-08-06 17:01:54 +0700

    fix error  chapter_8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
+
   before_action :set_locale
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def not_found?
+    render file: "#{Rails.root}/public/404.html", status: 403, layout: false
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by params[:id]
+    @user = User.find_by params :id
+    if  user.
   end
+
+  def show
+    if @user = User.find_by params[:id]
+    else
+      render "not find id"
+    end
+  end
+
+def show
+  @user = User.find_by_idparams[:id]
+  if @user.nil? 
+    render "error"
+  end
+end
+
 
   def new
     @user = User.new
@@ -10,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "users.create.title_text"
       redirect_to @user
     else
@@ -17,8 +34,9 @@ class UsersController < ApplicationController
     end
   end
 
+  private
   def user_params
-    params.require(:user).permit(:name, :email,
-      :password, :password_confirmation)
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
