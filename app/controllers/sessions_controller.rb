94b57e1e12b08e0423d9521c920f7d1dd42ed4ad
haphuoctuyen1997 <haphuoctuyen1997@gmail.com>
2018-08-06 17:01:54 +0700

    add chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,9 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    return render_login_page if params[:session].blank?
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
+      remembe_me user
       redirect_to user
     else
       flash.now[:danger] = t "users.create.invalid"
@@ -14,15 +13,14 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 
   private
 
-  def render_login_page
-    flash.now[:danger] = t "users.create.invalid"
-    render :new
-  end
+  def remembe_me user
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,22 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path "../../config/environment"
 require "rails/test_help"
-
 class ActiveSupport::TestCase
   fixtures :all
 
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].present?
+  end
+
+  def log_in_as user
+    session[:user_id] = user.id
+  end
+end
+class ActionDispatch::IntegrationTest
+  def log_in_as user, password: "password", remember_me: "1"
+    post login_path, params: {session: {email: user.email,
+                                        password: password,
+                                        emember_me: remember_me}}
   end
 end
