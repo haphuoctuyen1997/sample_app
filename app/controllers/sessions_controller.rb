02b1e47de97afc89df3ecb446041396c87f91f83
haphuoctuyen1997 <haphuoctuyen1997@gmail.com>
2018-08-06 17:01:54 +0700

    add chapter_8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
 
   def create
     return render_login_page if params[:session].blank?
-
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
-      render_login_page
+      flash.now[:danger] = t "users.create.invalid"
+      render :new
     end
   end
 
@@ -23,5 +23,6 @@ class SessionsController < ApplicationController
   def render_login_page
     flash.now[:danger] = t "users.create.invalid"
     render :new
+  end
   end
 end
